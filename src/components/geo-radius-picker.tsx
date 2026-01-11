256dc9ff3f51788cb86e753a607fd8f24016c004
Guru Preetam Bodapati <gurupreetam.ai@gmail.com>
2026-01-11 14:06:27 +0000

    Try fixing this error: `Console Error: AJAXError: (403): https://api.map

--- src/components/geo-radius-picker.tsx
+++ src/components/geo-radius-picker.tsx
@@ -263,7 +263,28 @@ export function GeoRadiusPicker() {
           }
         }}
         style={{ width: "100%", height: "100%" }}
-        mapStyle="https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL"
+        mapStyle={{
+            'version': 8,
+            'sources': {
+                'osm-tiles': {
+                    'type': 'raster',
+                    'tiles': [
+                        'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                        'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                        'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                    ],
+                    'tileSize': 256,
+                    'attribution': '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                }
+            },
+            'layers': [
+                {
+                    'id': 'osm-tiles',
+                    'type': 'raster',
+                    'source': 'osm-tiles'
+                }
+            ]
+        }}
       >
         <Source id="radius-circle" {...circleSource}>
             <Layer {...circleLayer} />
